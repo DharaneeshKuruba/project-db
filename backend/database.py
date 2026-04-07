0096ed1513317af53cb8eec67d7dd71fc0f164ca
DharaneeshKuruba <dharaneeshkuruba.2005@gmail.com>
2026-04-07 12:20:00 +0530

    added IP

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:admin123@localhost:5432/project"
+DATABASE_URL = "postgresql://admin:admin123@172.25.81.34:5432/project"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
